fc4fbc0a7d59a36e58c518a2287e155eefa60afe
ggnett <matter1708@gmail.com>
2025-04-02 15:40:27 +0300

    make header

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './normalize.scss';
+import Header from './components/Header/Header';
+
+function App() {
+    return (
+        <div class="wrapper">
+            <Header />
+        </div>
+    );
+}
+
+export default App;
