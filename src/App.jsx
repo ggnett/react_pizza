e064cfa5e419743c0ca6eaa596bfdbf750bef8d4
ggnett <matter1708@gmail.com>
2025-04-03 02:00:09 +0300

    make navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './normalize.scss';
 import Header from './components/Header/Header';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
     return (
         <div class="wrapper">
             <Header />
+            <NavBar />
         </div>
     );
 }
