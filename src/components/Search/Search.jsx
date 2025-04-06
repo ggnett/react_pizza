9ac82ddde7cb0da78174c7eeb42d081484e9696c
ggnett <matter1708@gmail.com>
2025-04-06 21:33:38 +0300

    make bounce

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -3,20 +3,36 @@ import React from 'react';
 import style from './Search.module.scss';
 import { useDispatch } from 'react-redux';
 import { filterUpd } from '../../redux/slices/searchSlice';
+import debounce from 'lodash.debounce';
 
 export default function Search() {
     const dispatch = useDispatch();
 
     const [inpValue, setInpValue] = React.useState('');
 
+    const inputRef = React.useRef();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    const filUpd = React.useCallback(
+        debounce((e) => {
+            dispatch(filterUpd(`&title=${e}`));
+        }, 1000),
+        []
+    );
+
     return (
         <div className={style.root}>
             <img className={style.search} src="./img/search.svg" alt="search" />
             <input
+                ref={inputRef}
                 value={inpValue}
                 onChange={(i) => {
                     setInpValue(i.target.value);
-                    dispatch(filterUpd(`&title=${i.target.value}`));
+                    // debounce(() => {
+                    //     dispatch(filterUpd(`&title=${i.target.value}`));
+                    //     console.log('event');
+                    // }, 200);
+                    filUpd(i.target.value);
                 }}
                 type="text"
                 placeholder="Поиск..."
@@ -25,6 +41,7 @@ export default function Search() {
                 onClick={() => {
                     setInpValue('');
                     dispatch(filterUpd(''));
+                    inputRef.current.focus();
                 }}
                 className={style.cross}
                 src="./img/cross.svg"
